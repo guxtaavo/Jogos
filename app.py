ccee63e0ad6da082de16728bdcc949a9e55cc94c
Gustavo Nunes Lopes <gustaavo.nunesl@gmail.com>
2024-02-07 09:50:42 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,7 @@
+from modelos.menu import Menu
+
+def main():
+    Menu.exibir_o_menu()
+
+if __name__ == '__main__':
+    main()
---
+++ modelos/menu.py
@@ -0,0 +1,13 @@
+class Menu:
+
+    def exibir_a_mensagem_inicial():
+        print('*'*28)
+        print('▀▄▀▄▀▄★彡[ᴊᴏɢᴏꜱ]彡★▀▄▀▄▀▄')
+        print('*'*28)
+
+    def exibir_as_opcoes():
+        pass
+
+    @staticmethod
+    def exibir_o_menu():
+        Menu.exibir_a_mensagem_inicial()
